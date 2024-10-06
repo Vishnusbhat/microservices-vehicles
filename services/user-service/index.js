1d6e68d61b86255f2be3739b6cf01b07eaf75f61
Vishnusbhat <vishnusbhat3@gmail.com>
2024-10-07 01:43:50 +0530

    Initial commit using CLI

---
+++ services/user-service/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+
+const port = 3001;
+
+app.listen(port, () => {
+    console.log(`Service User: Listening on port ${port}`);
+})
