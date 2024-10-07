bec34577cbd378e2356f1018aecfcf230937b5ba
Vishnusbhat <vishnusbhat3@gmail.com>
2024-10-08 00:57:35 +0530

    authentication function written successfully. need to add all the necessary endpoints.

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -4,8 +4,31 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+require('dotenv').config();
 const port = 3001;
+const secret = '10';
 
+const auth = ( req, res, next ) => {
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    if (!authHeader) res.status(501).send('auth header missing!');
+    const token = authHeader.split(' ')[0];
+    const user = jwt.verify(authHeader, '10');
+    console.log(user);
+    next();
+}
+
+app.post('/user/login', (req, res) => {
+    console.log(req.body);
+    const user = req.body;
+    const token = jwt.sign(user, secret, {expiresIn: '10m'});
+    console.log('token sent.');
+    return res.send(token);
+});
+
+app.get('/user/login', auth, (req, res) => {
+    console.log('you have been authenticated.');
+});
 app.listen(port, () => {
     console.log(`Service User: Listening on port ${port}`);
 });
