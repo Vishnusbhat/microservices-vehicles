8744ca69122572b4cc7a6a8387d61f3eb9bba115
Vishnusbhat <vishnusbhat3@gmail.com>
2024-10-10 00:41:59 +0530

    done implementing delete and update functions.

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -80,58 +80,96 @@ app.use((err, req, res, next) => {
     next();
 });
 
-const auth = ( req, res, next ) => {
+const auth = async ( req, res, next ) => {
+    console.log('Auth function running.');
     const authHeader = req.headers['authorization'];
     console.log(authHeader);
     if (!authHeader) res.status(501).send('auth header missing!');
     const token = authHeader.split(' ')[0];
     const user = jwt.verify(authHeader, secret);
+    const fullUserDetails = await User.findOne({email: user.email});
+    console.log(fullUserDetails);
+    req.user = fullUserDetails;
     console.log(user);
+    console.log('Auth function terminated.');
     next();
-}
-
+}//fuction that authenticates each API calls
 
 app.post('/user/signup', async (req, res) => {
     console.log(req.body);
     try {
         const { name, email, password, phone, address } = req.body;
-
         const user = await User.findOne({ email });
         if (user) {
             console.log('User already exists: ' + user);
             return res.status(401).json('User found! Proceed to login.');
         }
-
         const newUser = new User({ name, email, password, phone, address });
-        await newUser.save();
+        await newUser.save();//new user is added to the collection.
         console.log('New user created.');
-
         const token = jwt.sign({ name, email }, secret, { expiresIn: '30m' });
         console.log('Token created at the server.');
-        return res.json({ token });
-
+        return res.json({ token, "message": "Signup successful." });
     } catch (error) {
         console.log('Failed to send the token to the client: ' + error.message);
         return res.status(500).json({ error: 'Internal Server Error' });
     }
-});
+});//a new user is created if not previously existing in the collection. A new token is issued after creation of the user.
 
-app.post('/user/:vehicle', auth, async (req, res) => {
+app.post('/user/login', async (req, res) => {
+    console.log('Trying to login.');
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        console.log('User does not exist. Please Signup.');
+        return res.status(404).json('User not found!');
+    }
+    const token = jwt.sign({
+        'name': user.name,
+        'email': user.email
+    }, secret, {expiresIn: '30m'});
+    console.log('Login successful.');
+    return res.json({token, 'message': 'login successful.'});
+});//used to login to an existing user. A new token will be returned.
+
+app.post('/user/:vehicle(\\d+)', auth, async (req, res) => {
     const token = req.headers['authorization'];
     const user = jwt.verify(token, secret);
     const vehicleID = req.params.vehicle;
+    const userID = User.findOne({email: user.email});
     await User.updateOne(
         { email: user.email },
         { $addToSet: {vehicles: vehicleID}}
     )
-    return res.json('VehicleID ' + vehicleID + ' has been added to user with the email ' + user.email);
+    return res.status(200).json({'userID' : userID});
+});//when a new vehicle is added by a user, the vehicle service will talk to user service to get the user ID and also provide the vehicle ID to be added into the 'Vehicles' array in the user schema.
+
+app.get('/user/details', auth, (async ( req, res ) => {
+    console.log('Sending user details...');
+    const user = req.user;
+    console.log(user);
+    return res.status(201).json({ user });
+}));//to return the full structure of the user object when the user requests for it. Cannot be accessed by other users.
+
+app.get('/user/list', async ( req, res ) => {
+    const user = await User.find({}, {name: 1, email: 1});
+    return res.json({ user });
+});//to return the list of all the users 'Name', 'Email' and ObjID. No autherisation is needed.
+
+app.delete('/user/delete', auth, async ( req, res ) => {
+    const user = req.user;
+    await User.deleteOne({ _id: user._id });
+    return res.json("Deleted User: " + user.name);
+});//to delete the user that is making the request. The id will be collected through the header in the auth function.
+
+app.put('/user/update', auth, async ( req, res ) => {
+    const user = req.user;
+    const updateData = req.body;
+    await User.updateOne({_id: user._id }, { $set: updateData });
+    return res.status(201).json({"message": "Updated the data:", "Updated data" : updateData});
 });
 
 
-app.get('/user/login', auth, (req, res) => {
-    console.log('you have been authenticated.');
-    res.json('authenticated.');
-});
 app.listen(port, () => {
     console.log(`Service User: Listening on port ${port}`);
 });
